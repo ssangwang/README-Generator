282495c3249c0027c0da443d452c0f6076340839
Code <sangw009@umn.edu>
2022-11-01 17:39:43 -0500

    finished with the README generator, need to finish final touches

---
+++ README.md
@@ -0,0 +1,17 @@
+# ReadME Generator 
+## Installation 
+In order to use the tool, clone the repository from GitHub into your PC and from there use NODE to start up the index.js
+## Description 
+This project was made for develeopers who are tired of typing their own READMEs, so they can have an automated source 
+## Usage 
+This project would be used by developers that need to create READMEs
+## Tests 
+No tests performed.
+## Contributing 
+None
+## License
+MIT License
+## GitHub 
+undefined
+## Email 
+undefined
--- index.js
+++ index.js
@@ -19,39 +19,74 @@ inquirer
       name: 'Installation',
     },
     {
-        type: 'input',
-        message: 'How would this project be used in a real world setting?',
-        name: 'Usage',
-      },
-      {
-        type: 'input',
-        message: 'Were there any contributing developers?',
-        name: 'Contributing',
-      },
-      {
-        type: 'input',
-        message: 'Please provide tests if any were performed',
-        name: 'Tests',
-      },
-      {
-        type: 'input',
-        message: 'Which license is this project covered?',
-        name: 'License',
-      },
-      {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'github',
-      },
-      {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email',
-      },
+      type: 'input',
+      message: 'How would this project be used in a real world setting?',
+      name: 'Usage',
+    },
+    {
+      type: 'input',
+      message: 'Were there any contributing developers?',
+      name: 'Contributing',
+    },
+    {
+      type: 'input',
+      message: 'Please provide tests if any were performed',
+      name: 'Tests',
+    },
+    {
+      type: 'input',
+      message: 'Which license is this project covered?',
+      name: 'License',
+    },
+    {
+      type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'What is your email?',
+      name: 'email',
+    },
   ])
   .then((response) => {
     const string = JSON.stringify(response);
     const answerString = JSON.parse(string);
     console.log(answerString);
-    const {Title, Description, Installation, Usage, Contributing, Tests, License, Github, Email} = answerString;
+    const { Title, Description, Installation, Usage, Contributing, Tests, License, Github, Email } = answerString;
+    fs.writeFile('README.md', `# ${Title}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+    //Description
+fs.appendFile('README.md', `
+## Description 
+${Description}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+//Installation
+fs.appendFile('README.md', `
+## Installation 
+${Installation}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+//Usage
+fs.appendFile('README.md', `
+## Usage 
+${Usage}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+//Contributing
+fs.appendFile('README.md', `
+## Contributing 
+${Contributing}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+//Tests
+fs.appendFile('README.md', `
+## Tests 
+${Tests}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+//License
+fs.appendFile('README.md', `
+## License
+${License}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+//GitHub
+fs.appendFile('README.md', `
+## GitHub 
+${Github}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+//Email
+fs.appendFile('README.md', `
+## Email 
+${Email}`, (err) => err ? console.error(err) : console.log('Commit logged!'))
+
   });
+
